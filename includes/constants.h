ad97382561ad3efa1103361b585838f2530ead62
Laura Slayton <laura.slayton.19@cnu.edu>
2020-09-24 19:05:13 -0400

    started 2

---
+++ includes/constants.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const int 			EMPTY			 				=  0;
+const int 			SUCCESS			 				=  0;
+const int 			FAIL							=  SUCCESS - 1;
+const int 			COULD_NOT_OPEN_FILE				=  SUCCESS - 2;
+const int 			NO_DATA_TO_WORK_ON				=  SUCCESS - 3;
+const int			UNIMPLEMENTED					=  SUCCESS - 4;
+const int			UNINITIALIZED					=  SUCCESS - 5;
+
+#endif
